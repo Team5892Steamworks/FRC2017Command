bbf53d83d21ddf69882162120c00e5844a22d905
Kai Page <kaibug@gmail.com>
2017-03-26 15:13:28 -0500

    Updated the numbers for EncoderScoreGearAuto.

--- src/org/usfirst/frc/team5892/robot/commands/autonomous/EncoderScoreGearAuto.java
+++ src/org/usfirst/frc/team5892/robot/commands/autonomous/EncoderScoreGearAuto.java
@@ -6,8 +6,8 @@ public class EncoderScoreGearAuto extends EncoderAuto {
     public EncoderScoreGearAuto(boolean goForHopper, Position position) {
     	double dir = position == Position.RIGHT ? 1 : -1;
     	
-    	addSequential(new EncoderAutonomousDriveLeg(0, 0.2, 0, distToBaseline + linearInch(20)));
-    	addSequential(new EncoderAutonomousDriveLeg(0, 0, dir*0.4, 75));
+    	addSequential(new EncoderAutonomousDriveLeg(0, 0.2, 0, distToBaseline + linearInch(22)));
+    	addSequential(new EncoderAutonomousDriveLeg(0, 0, dir*0.4, 82));
     	//addSequential(new VisionGearAlign());
     	addSequential(new EncoderAutonomousDriveLeg(0, 0.2, 0, linearInch(24)));
     	
@@ -17,4 +17,8 @@ public class EncoderScoreGearAuto extends EncoderAuto {
     		//addSequential(new EncoderAutonomousDriveLeg(0, 0, dir*0.3, ))
     	}
     }
+    /*
+     * 3/25 - 20, 75, 24 worked
+     * 3/26 - 22, 82, 24 worked
+     */
 }
